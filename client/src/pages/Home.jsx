b13f2c329304ae1ee565685a1b85df465db6efa9
Sagar Bharati <bharatisagar678@gmail.com>
2024-10-03 22:46:32 +0530

    fixed late render problem

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import { motion, AnimatePresence } from 'framer-motion';
 import { useSnapshot } from 'valtio';
-
+import { useState,useEffect } from 'react';
 import state from '../store';
 import { CustomButton } from '../components';
 import {
@@ -12,7 +12,17 @@ import {
 
 const Home = () => {
   const snap = useSnapshot(state);
+  const [showComponent, setShowComponent] = useState(false);
 
+  useEffect(() => {
+    // Set a timer for 2 seconds (2000 milliseconds)
+    const timer = setTimeout(() => {
+      setShowComponent(true);  // Show the component after 2 seconds
+    }, 2000);
+
+    // Cleanup timer on component unmount
+    return () => clearTimeout(timer);
+  }, []);
   return (
     
     <AnimatePresence> 
@@ -34,13 +44,22 @@ const Home = () => {
               <p className="max-w-md font-normal text-gray-600 text-base">
               Create your unique and exclusive shirt with our brand-new 3D customization tool. <strong>Unleash your imagination</strong>{" "} and define your own style.
               </p>
-
-              <CustomButton 
+              {showComponent ? (
+        <CustomButton 
+        type="filled"
+        title="Customize It"
+        handleClick={() => state.intro = false}
+        customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+      /> // Render the button after 2 seconds
+      ) : (
+        <p></p>  // Optional: Show a loading message until the button appears
+      )}
+              {/* <CustomButton 
                 type="filled"
                 title="Customize It"
                 handleClick={() => state.intro = false}
                 customStyles="w-fit px-4 py-2.5 font-bold text-sm"
-              />
+              /> */}
             </motion.div>
           </motion.div>
         </motion.section>
